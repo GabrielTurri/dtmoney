e865c0fd5e8b22ee54190af797d80cba30999dfb
Gabriel Turri <ga-briel.t.a@hotmail.com>
2022-08-03 11:00:40 -0300

    setting the assets and styled-components

---
+++ src/styles/global.ts
@@ -0,0 +1,49 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #f0f2f5; //cinza-branco
+    --red: #E52E4D;
+    --blue: #5429cc;
+
+    --blue-light: #6933ff;
+
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+
+    --shape: #FFFFFF;
+  }
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  /* font-size: 16px  (Desktop)*/
+  html{
+    @media (max-width: 1080px) {
+      font-size: 93.75; //15px
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%; //14px
+    }
+    /* Aqui utilizo a porcentagem por meios de 
+    acessibilidade para quem muda as configuraçoes
+     de fonte do celular/ desktop */
+  }
+
+  body{
+    background: --background;
+    -webkit-font-smoothing: antialised;
+  }
+  button {
+    cursor: pointer
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`
