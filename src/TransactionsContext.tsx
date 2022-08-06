79433172f38fc156fbb87c05d69ef89a7b158f14
Gabriel Turri <ga-briel.t.a@hotmail.com>
2022-08-06 17:18:16 -0300

    finished the insertion

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -13,7 +13,7 @@ interface Transaction {
 
 interface TransactionsContextData {
   transactions: Transaction[];
-  createTransaction : (transaction: TransactionInput) => void;
+  createTransaction : (transaction: TransactionInput) => Promise<void>;
 }
 
 type TransactionInput = Omit<Transaction, 'id'| 'createdAt'>
@@ -32,9 +32,17 @@ export function TransactionsProvider({ children }:TransactionsProviderProps) {
     .then(response => setTransactions(response.data.transactions))
   }, []);
 
-  function createTransaction(transaction: TransactionInput){
+  async function createTransaction(transactionInput: TransactionInput){
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date(),
+    })
+    const { transaction } = response.data
 
-    api.post('/transactions', transaction)
+    setTransactions([
+      ...transactions,
+      transaction,
+    ]);
   }
 
   return (
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -23,15 +23,21 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
   const [type, setType] = useState('deposit');
 
   // Cadastro da nova transação
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    createTransaction({
+    await createTransaction({
       title,
       amount,
       category,
       type
     })
+
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    setType('deposit');
+    onRequestClose();
   }
 
 
