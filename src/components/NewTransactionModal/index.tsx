36dea7150475349bac0dcfb696934327b6d46b10
Gabriel Turri <ga-briel.t.a@hotmail.com>
2022-08-06 16:08:27 -0300

    creating transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -15,23 +15,23 @@ interface NewTransactionModalProps{
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionModalProps){
-  const transactions = useContext(TransactionsContext);
+  const { createTransaction } = useContext(TransactionsContext);
 
   const [title, setTitle] = useState('');
-  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
+  // Cadastro da nova transação
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
-    const data ={
-      title,
-      value,
-      category, 
-      type
-    };
 
-    api.post('/transactions', data)
+    createTransaction({
+      title,
+      amount,
+      category,
+      type
+    })
   }
 
 
@@ -65,8 +65,8 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
         <input 
           placeholder="Valor"
           type="number"
-          value={value}
-          onChange={event => setValue(Number(event.target.value))}
+          value={amount}
+          onChange={event => setAmount(Number(event.target.value))}
         />
 
         <TransactionTypeContainer>
